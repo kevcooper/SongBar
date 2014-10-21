cb3907a64959090532d5593b3c2239f269396ced
Kevin Cooper <k.coopr@gmail.com>
2014-10-21 13:05:45 -0700

    Initial commit from local

---
+++ SongBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SongBar
+//
+//  Created by Kevin Cooper on 10/21/14.
+//  Copyright (c) 2014 corpe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
