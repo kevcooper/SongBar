550161491a9d3debc3dd1cb12589bf20cdd951cf
Kevin Cooper <k.coopr@gmail.com>
2014-10-22 06:43:56 -0700

    Status bar mostly working

--- SongBar/AppDelegate.swift
+++ SongBar/AppDelegate.swift
@@ -7,26 +7,53 @@
 //
 
 import Cocoa
+import AppKit
+import ScriptingBridge
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
     @IBOutlet weak var window: NSWindow!
     
     var sysBar: NSStatusItem!
+    var iTunes: AnyObject!
+    
+    //magic number
     var variableStatusItemLength: CGFloat = -1;
-
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         
-        sysBar = NSStatusBar.systemStatusBar().statusItemWithLength(variableStatusItemLength);
-        sysBar.title = "hello";
-
+        sysBar = NSStatusBar.systemStatusBar().statusItemWithLength(100);
+        iTunes = SBApplication.applicationWithBundleIdentifier("com.apple.iTunes");
+        
+        updateStatusBar();
+        
+        NSDistributedNotificationCenter.defaultCenter().addObserver(self,
+            selector: "updateStatusBar",
+            name: "com.apple.iTunes.playerInfo",
+            object: nil);
         
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        NSDistributedNotificationCenter.defaultCenter().removeObserver(self);
     }
-
-
+    
+    func updateStatusBar(){
+        let track: iTunesTrack = iTunes.currentTrack;
+        
+        let name: String = (track.name != nil) ? track.name : "";
+        let artist: String = (track.artist != nil) ? track.artist : "";
+        
+        if(artist != "" && name != ""){
+            sysBar.title! = name + " by " + artist;
+        }else{
+            sysBar.title! = "SongBar";
+        }
+        
+        
+    }
+    
+    
 }
---
+++ SongBar/SongBar-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SongBar-Bridging-Header.h
+//  SongBar
+//
+//  Created by Kevin Cooper on 10/21/14.
+//  Copyright (c) 2014 corpe. All rights reserved.
+//
+
+#ifndef SongBar_SongBar_Bridging_Header_h
+#define SongBar_SongBar_Bridging_Header_h
+
+#endif
+
+#import "iTunes.h"
