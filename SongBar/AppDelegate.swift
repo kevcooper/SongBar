7398d962bd33f6ad4dd0237be1f721463a477119
Justin Oakes <justin@justinoak.es>
2015-06-19 20:00:23 -0600

    made initial track artist combo string  format consistant with subsequent format. added Spotify support. set up for grabbing initial data when launching for both services

--- SongBar/AppDelegate.swift
+++ SongBar/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var sysBar: NSStatusItem!
     var iTunes: AnyObject!
-    
+    var Spotify: AnyObject!
     //magic number
     var variableStatusItemLength: CGFloat = -1;
     
@@ -28,13 +28,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         sysBar = NSStatusBar.systemStatusBar().statusItemWithLength(variableStatusItemLength);
         sysBar.menu = menu;
         iTunes = SBApplication.applicationWithBundleIdentifier("com.apple.iTunes");
+        Spotify = SBApplication.applicationWithBundleIdentifier("com.spotify.client")
         
         updateStatusBar();
         
         NSDistributedNotificationCenter.defaultCenter().addObserver(self,
-            selector: "updateFromNotification:",
-            name: "com.apple.iTunes.playerInfo",
-            object: nil);
+                                                                    selector: "updateiTuensFromNotification:",
+                                                                        name: "com.apple.iTunes.playerInfo",
+                                                                      object: nil);
+        NSDistributedNotificationCenter.defaultCenter().addObserver(self,
+                                                                    selector: "updateSpotifyFromNotification:",
+                                                                        name: "com.spotify.client.PlaybackStateChanged",
+                                                                      object: nil)
         
     }
     
@@ -43,13 +48,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func updateStatusBar(){
+
         let track: iTunesTrack = iTunes.currentTrack;
+        let spotifyTrack: SpotifyTrack = Spotify.currentTrack
         
         let name: String = (track.name != nil) ? track.name : "";
         let artist: String = (track.artist != nil) ? track.artist : "";
         
+        let spotifyName: String = spotifyTrack.name != nil ? spotifyTrack.name : ""
+        let spotifyArtist: String = spotifyTrack.artist != nil ? spotifyTrack.artist : ""
+        
         if(artist != "" && name != ""){
-            sysBar.title! = name + " by " + artist;
+            sysBar.title! = name + " - " + artist;
+        }else if spotifyArtist != "" && spotifyName != ""{
+            sysBar.title = "\(spotifyName) - \(spotifyArtist)"
         }else{
             sysBar.title! = "SongBar";
         }
@@ -57,7 +69,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
     }
     
-    func updateFromNotification(aNotification: NSNotification){
+    func updateiTuensFromNotification(aNotification: NSNotification){
         let info = aNotification.userInfo! as NSDictionary;
         
         if(info.objectForKey("Name") != nil && info.objectForKey("Artist") != nil){
@@ -70,9 +82,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    func updateSpotifyFromNotification(aNotification: NSNotification){
+                let info = aNotification.userInfo! as NSDictionary;
+        if info["Name"] != nil && info["Artist"] != nil {
+            let name: String = info["Name"] as! String
+            let artist: String = info["Artist"] as! String
+            
+            sysBar.title = "\(name) - \(artist)"
+        } else{
+            sysBar.title! = "SongBar";
+        }
+    }
+    
     @IBAction func playPause(sender: AnyObject) {
         iTunes.playpause();
     }
     
+
+}
+
+enum Service {
+    case iTunes
     
+    case spotify
 }
